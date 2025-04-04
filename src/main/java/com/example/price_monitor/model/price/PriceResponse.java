91c0efcbb7fb8159531716a7d543a267e373dc10
postwo <codo7717@naver.com>
2025-04-04 19:40:20 +0900

    api 데이터를 PriceResponce에서 커스텀 구현

---
+++ src/main/java/com/example/price_monitor/model/price/PriceResponse.java
@@ -0,0 +1,21 @@
+package com.example.price_monitor.model.price;
+
+import com.example.price_monitor.model.coinbase.SpotPriceResponse;
+
+public record PriceResponse(Double amount, String base, String currency) {
+    public static PriceResponse from(SpotPriceResponse spotPriceResponse) {
+        var amount = Double.parseDouble(spotPriceResponse.data().amount());
+        var base = spotPriceResponse.data().base();
+        var currency = spotPriceResponse.data().currency();
+
+        return new PriceResponse(amount, base, currency);
+    }
+
+    //여기서는 본래 api하고 다르게 data를 벗기고 그안쪽에 있는 데이터만 사용할거다
+    //ex)
+    //    {
+        //    "amount": "83823.155",
+        //    "base": "BTC",
+        //    "currency": "USD"
+    //    }
+}
